e81e84cb5da662e78df9d172664a4ac792020dc9
Nick Rizzo <nickmike55@gmail.com>
2020-01-22 22:39:22 -0500

    initial commit with basic working setup script to create virtual environment to pip flask

---
+++ setup.sh
@@ -0,0 +1,26 @@
+# create virtual environment to install desired packages (i.e. flask)
+THIS_FILE_DIR="$(readlink -fm $0/..)"
+virtualEnvironName="emailEnv"
+virtualEnvironDir=$THIS_FILE_DIR/$virtualEnvironName
+pipLocation="" # make global
+
+# check OS... (decide how to activate virtual environment)
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    # windows
+    py -m venv $virtualEnvironDir # actually create the virtual environment
+    $virtualEnvironDir/Scripts/activate
+    pipLocation=$virtualEnvironDir/Scripts/pip3.exe
+
+else
+    # linx
+    # sudo apt update
+    # sudo apt install python3-pip
+    # sudo apt-get install python3-venv
+    python3 -m venv $virtualEnvironDir # actually create the virtual environment
+    source $virtualEnvironDir/bin/activate
+    pipLocation=$virtualEnvironDir/bin/pip3
+    $pipLocation install numpy
+fi
+
+# now pip necessary packages
+$pipLocation install flask

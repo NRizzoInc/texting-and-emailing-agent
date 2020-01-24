4283fbb3f88e88c20f6f6301e1299d020d759e62
Nick Rizzo <nickmike55@gmail.com>
2020-01-23 21:51:14 -0500

    Basic architecture of web app is established

--- webApp.py
+++ webApp.py
@@ -10,15 +10,24 @@ class WebApp():
     def __init__(self):
         self.host_ip = self.getIP()
         self.host_port = '9999'
-        self.host_address = 'https://' + self.host_ip + ':' + self.host_port +'/'
+        self.host_address = 'http://' + self.host_ip + ':' + self.host_port
         self.app = Flask(__name__)
         self.app.static_folder = "templates/stylesheets" # change location of where the css stylesheets are
+        self.sites = {
+            "landingpage"   :   '/',
+            "textpage"      :   '/textpage',
+            "emailpage"     :   '/emailpage',
+            "aboutpage"     :   '/aboutpage',
+            "sidebarpage"   :   '/sidebarpage'
+        }
+        self.debugOn = True
 
         # create all sites to begin with
         self.generateSites()
+        self.printSites() # will only print if debug mode is on
 
         # start up the web app
-        self.app.run(host=self.host_ip, port=self.host_port, debug=True)
+        self.app.run(host=self.host_ip, port=self.host_port, debug=self.debugOn)
     
     def getIP(self):
         hostname = socket.gethostname()
@@ -30,10 +39,40 @@ class WebApp():
             This function is a wrapper around all the other functions which actually create pages.
             Hence, by calling this function all sites will be initialized.
         '''
-
-        @self.app.route('/')
+        # wrap pages in generateSites function so that 'self' can be used
+        @self.app.route(self.sites['landingpage'])
         def createMainPage():
-            return render_template("mainPage.html", title="Texting Application Main Page")
+            return render_template("mainPage.html", title="Texting Application Main Page", links=self.sites) + self._renderSidebar()
+
+        @self.app.route(self.sites['textpage'])
+        def createTextPage():
+            return render_template("textPage.html", title="Texting Application Texting Page", links=self.sites) + self._renderSidebar()
+
+        @self.app.route(self.sites['emailpage'])
+        def createEmailPage():
+            return render_template("emailPage.html", title="Texting Application Email Page", links=self.sites) + self._renderSidebar()
+
+        @self.app.route(self.sites['aboutpage'])
+        def createAboutPage():
+            return render_template("aboutPage.html", title="Texting Application About Page", links=self.sites) + self._renderSidebar()
+    
+    def printSites(self):
+        '''
+            Helper function to tell user the location of each site
+        '''
+        if (self.debugOn):
+            print("\nAll the created sites are: \n")
+            for site in self.sites.keys():
+                print("{0}{1}".format(self.host_address, self.sites[site]))
+            print() # newline
+
+    def _renderSidebar(self):
+        '''
+            Helper function that renders the sidebar for use by the actual sites
+        '''
+        return render_template("sidebar.html", links=self.sites)
+
+
 
 if __name__ == "__main__":
     ui = WebApp()

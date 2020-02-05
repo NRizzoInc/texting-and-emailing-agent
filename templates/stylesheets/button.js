cf79ae5bbd50ca210a5114390268ba9928ee8d60
Nick Rizzo <nickmike55@gmail.com>
2020-02-04 21:56:19 -0500

    When buttons are clicked they now disappear

--- templates/stylesheets/button.js
+++ templates/stylesheets/button.js
@@ -1,5 +1,9 @@
 // This file contains all the button commands
 function buttonPostRequest(pageToRequest) {
+    // trigger new page and hide buttons
     $.post(pageToRequest, function onSuccess () {
+        // document.getElementsByClassName('.button-wrapper').style.visibility = 'hidden'
+        console.log(document.getElementsByClassName('button-wrapper'))
+        document.getElementsByClassName('button-wrapper')[0].style.visibility = "hidden"
     })
 }

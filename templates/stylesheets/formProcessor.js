8797cf82bac09aacf2e53fd04c5963d6a99af0c5
Nick Rizzo <nickmike55@gmail.com>
2020-02-06 22:23:50 -0500

    Exclude submit button and include text & input tags

--- templates/stylesheets/formProcessor.js
+++ templates/stylesheets/formProcessor.js
@@ -1,10 +1,27 @@
 function parseForm(formId, formLink) {
     // loop through form to get form key:value pairs 
     let form = document.getElementById(formId);
-    console.log(form)
-    console.log(JSON.stringify(form['1']))
-    let formData = JSON.stringify(form);
+    let inputTags = form.getElementsByTagName('input');
+    let textTags = form.getElementsByTagName('textarea');
+    let inputs = [];
+    inputs.push(inputTags);
+    inputs.push(textTags);
+    console.log(inputs)
+    
+    let formData = {}; // declare json
+    for (let tag of inputs) { // iterate through all possible tags
+        for (let key of tag) { // iterate through all matches of that tag
+            if (key.id != "Submit-Button") {
+                console.log("Id: " + key.id + "\nValue: " + key.value)
+                let value = document.getElementById(key.id).value
+                formData[key.id] = value
+            }
+        }
+    }
+    formData = JSON.stringify(formData)
     console.log(formData)
+
+    // POST data to website
     $.ajax({
         url: formLink,
         type: 'POST',

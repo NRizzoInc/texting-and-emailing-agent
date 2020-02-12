075292d120cabf8caf665fe63f969b17a1114b31
Nick Rizzo <nickmike55@gmail.com>
2020-02-11 19:20:34 -0500

    Fixed callback issue

--- templates/stylesheets/button.js
+++ templates/stylesheets/button.js
@@ -4,13 +4,13 @@ function buttonPostRequest(pageToRequest, id) {
     $.post(pageToRequest, function onSuccess () {
         document.getElementsByClassName('button-wrapper')[0].style.display = "none"
         document.getElementById('Texting-Form-Wrapper').style.display = "block"
+        
+        if (id == 'text-receive-button') {
+            document.getElementById('message-container').style.display = 'none'
+            // TODO: show text results (terminal on page)
+        } 
+        else if (id == 'text-send-button') {
+            
+        }
     })
-
-    if (id == 'text-receive-button') {
-        document.getElementById('message-container').style.display = 'none'
-        // TODO: show text results (terminal on page)
-    } 
-    else if (id == 'text-send-button') {
-
-    }
 }

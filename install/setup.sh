153741e17e9c5eb1810a30418258ce2ff5333a9d
Nick Rizzo <nickmike55@gmail.com>
2020-07-13 04:09:41 -0400

    [fix] Better installation for linux -- version specific

--- install/setup.sh
+++ install/setup.sh
@@ -8,21 +8,35 @@ pythonLocation=${virtualEnvironDir}/Scripts/python.exe
 pipLocation="" # make global
 
 # check OS... (decide how to activate virtual environment)
-echo "#1 Creating Virtual Environment"
+echo "#1 Setting up virtual environment"
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
     # windows
+    echo "#1.1 Creating Virtual Environment"
     py -m venv $virtualEnvironDir # actually create the virtual environment
     $virtualEnvironDir/Scripts/activate
     pipLocation=$virtualEnvironDir/Scripts/pip3.exe
 
 else
     # linx
-    # sudo apt update
-    # sudo apt install python3-pip
-    # sudo apt-get install python3-venv
-    python3 -m venv $virtualEnvironDir # actually create the virtual environment
-    source $virtualEnvironDir/bin/activate
-    pipLocation=$virtualEnvironDir/bin/pip3
+    # needed to get specific versions of python
+    pythonVersion=3.7
+    pythonName=python${pythonVersion}
+    echo "#1.1 Adding python ppa"
+    sudo add-apt-repository -y ppa:deadsnakes/ppa
+
+    echo "#1.2 Updating..."
+    sudo apt update -y
+
+    echo "#1.3 Upgrading..."
+    sudo apt upgrade -y
+    sudo apt install -y \
+        ${pythonName} \
+        ${pythonName}-venv
+
+    echo "#1.4 Creating Virtual Environment"
+    ${pythonName} -m venv ${virtualEnvironDir} # actually create the virtual environment
+    source ${virtualEnvironDir}/bin/activate
+    pipLocation=${virtualEnvironDir}/bin/pip${pythonVersion}
 fi
 
 # update pip to latest

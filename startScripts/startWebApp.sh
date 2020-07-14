5b16a9094435aec9a548740d2cc0f14a1427355d
Nick Rizzo <nickmike55@gmail.com>
2020-07-13 22:42:39 -0400

    [feat] Added flags to startWebApp.sh to specific port to run on

--- startScripts/startWebApp.sh
+++ startScripts/startWebApp.sh
@@ -10,6 +10,41 @@ backendPath=${rootDir}/backend
 srcPath=${backendPath}/src/webApp
 executePath=${srcPath}/webApp.py
 
+# CLI Flags
+print_flags () {
+    echo "Usage: startWebApp.sh"
+    echo "=========================================================================================================="
+    echo "Helper utility to start up the email web application"
+    echo "Starts up the virtual environment via bash, meaning user does not need to have python locally installed"
+    echo "How to use:" 
+    echo "To Start: ./startWebApp.sh [options]"
+    echo "To Stop: control+c script to stop the web app"
+    echo "=========================================================================================================="
+    echo "Available flags:"
+    echo "  -p,--port <desired port>: Choose the port to run on"
+    echo "  -h,--help : This messsage"
+    echo "=========================================================================================================="
+}
+
+# parse command line args
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -p | --port )
+            port="$2"
+            shift
+            ;;
+        -h | --help )
+            print_flags
+            exit 0
+            ;;
+        * )
+            echo "... Unrecognized command"
+            print_flags
+            exit 1
+    esac
+    shift
+done
+
 # check OS... (decide how to call python)
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
     # windows
@@ -19,4 +54,4 @@ else
     pythonLocation=${virtualEnvironDir}/bin/python # NOTE: don't use ".exe"
 fi
 
-${pythonLocation} ${executePath}
+${pythonLocation} ${executePath} --port ${port}

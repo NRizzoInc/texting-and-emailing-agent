b97c4a0d33f396e5b12a8f11cda0ebc6df509495
Nick Rizzo <nickmike55@gmail.com>
2020-07-15 03:41:05 -0400

    [feat] Start/stop scripts for mangoDB

---
+++ serviceScripts/startMangoDB.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Note: needs to be admin cmd or sudo
+
+[[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]] && isWindows=true || isWindows=false
+
+if [[ ${isWindows} == false ]]; then
+    if [ "$EUID" -ne 0 ]; then
+        echo "Please run as root ('sudo')"
+        exit
+    else # have sudo permissions
+        echo "IMPLEMENT LINUX START"
+        # linux start cmd
+    fi
+else # is windows
+    # || denotes "on fail"
+    net start MongoDB || echo "Run in Admin Command Prompt"
+fi

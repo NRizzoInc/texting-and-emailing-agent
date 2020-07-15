f5d63027b4e2162a524fb26e01662b5e6bd56c5f
Nick Rizzo <nickmike55@gmail.com>
2020-07-15 00:06:17 -0400

    [feat] Stub for python database manager

---
+++ backend/src/database/databaseManager.py
@@ -0,0 +1,16 @@
+"""
+    @file: This file contains the database helper class
+    @Note: Calling this with python will do nothing
+"""
+
+#------------------------------STANDARD DEPENDENCIES-----------------------------#
+
+#-----------------------------3RD PARTY DEPENDENCIES-----------------------------#
+
+
+class DatabaseManager():
+    def __init__(self):
+        """
+            This class is meant to help manage the database of users' information
+        """
+        pass # stub

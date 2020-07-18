b635f135b140818c300061f46b2dd0b62e6f76b7
Nick Rizzo <nickmike55@gmail.com>
2020-07-17 23:44:48 -0400

    [feat] Added command line args to the keyboard test program

--- backend/src/emailing/keyboardHandler.py
+++ backend/src/emailing/keyboardHandler.py
@@ -1,12 +1,13 @@
 """
     @File: Helper class for monitoring keyboard event
+    @Note: Return False for callbacks trigger stop monitoring
 """
 
 #------------------------------STANDARD DEPENDENCIES-----------------------------#
-
+import argparse # for CLI Flags
 
 #-----------------------------3RD PARTY DEPENDENCIES-----------------------------#
-from pynput.keyboard import Key, Controller, Listener # to monitor keyboard
+from pynput.keyboard import Key, KeyCode, Controller, Listener # to monitor keyboard
 
 #--------------------------------OUR DEPENDENCIES--------------------------------#
 
@@ -19,6 +20,7 @@ class KeyboardMonitor():
         """Helper function that generates function that stops the listener if 'stopKey' is pressed"""
         def __onPress(pressedKey):
             print(f"{pressedKey} pressed")
+            # have to stop on release or else leaves thread hanging
             # return not pressedKey == stopKey
         return __onPress
 
@@ -38,5 +40,22 @@ class KeyboardMonitor():
 
 # Test functionality
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Keyboard Handler & Monitoring Tester")
+    parser.add_argument(
+        "-k", "--stop-key",
+        metavar="<key to stop on>",
+        default=None,
+        required=False,
+        dest="stopKey",
+        help="The key to stop listening on",
+    )
+
+    # Actually Parse Flags (turn into dictionary)
+    # converts all '-' after '--' to '_'
+    args = vars(parser.parse_args())
+
+    stopKey = args["stopKey"]
+    print(f"Stopping on '{stopKey}' key")
+
     keyMonitor = KeyboardMonitor()
-    keyMonitor.initListener()
+    keyMonitor.initListener(stopKey=stopKey)
